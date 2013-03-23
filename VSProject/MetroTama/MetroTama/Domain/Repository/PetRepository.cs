e739e120c5b53176065590a803d0f3c0c5ab71b3
Dmitrijs Balcers <dmitrijs.balcers@gmail.com>
2013-03-23 14:21:35 +0200

    Added Pet and Stage repository

---
+++ VSProject/MetroTama/MetroTama/Domain/Repository/PetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroTama.Domain.Repository
+{
+    class PetRepository
+    {
+        public Pet GetPet() {
+            Pet pet = new Pet();
+            pet.PetId = 1;
+            pet.Name = "Tamo";
+            return pet;
+        }
+    }
+}
