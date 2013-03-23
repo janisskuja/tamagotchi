d06d0d5dfe7f3406e75623df7560b6a3612949a3
Lauris Radzevics <radzevics.lauris@gmail.com>
2013-03-23 14:26:19 +0200

    Created directory and GameObject service

---
+++ VSProject/MetroTama/MetroTama/Services/GameObjectService.cs
@@ -0,0 +1,17 @@
+﻿using MetroTama.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroTama.Services
+{
+    class GameObjectService
+    {
+        public static void UseObject(Pet temp_pet, int gameObjectId)
+        {
+            
+        }
+    }
+}
