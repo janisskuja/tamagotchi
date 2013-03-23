33a2eae8aa43bc951ca8f1e6eb9a2dd53d3341b0
Lauris Radzevics <radzevics.lauris@gmail.com>
2013-03-23 15:50:16 +0200

    Finished UseObject() function.
    Finished UseObject() function which updates stats for pet!

--- VSProject/MetroTama/MetroTama/Services/GameObjectService.cs
+++ VSProject/MetroTama/MetroTama/Services/GameObjectService.cs
@@ -1,4 +1,5 @@
 ﻿using MetroTama.Domain;
+using MetroTama.Domain.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +12,35 @@ namespace MetroTama.Services
     {
         public static void UseObject(Pet temp_pet, int gameObjectId)
         {
-
+            GameObjectRepository temp_GameObjectRepository = new GameObjectRepository();
+            GameObject temp_GameObject = temp_GameObjectRepository.GetGameObject(gameObjectId);
+           
             if (temp_pet.FavoriteFoodId == gameObjectId)
             {
                 // Encrease Fun
+                temp_pet.Fun += temp_GameObject.FunEffect;
             }
             else if (temp_pet.DislikedFoodId == gameObjectId)
             {
                 // Decrese Fun
+                temp_pet.Fun -= temp_GameObject.FunEffect;
             }
 
+            //Update Health
+            if(temp_GameObject.IsHealthy)
+            {
+                temp_pet.Healt += temp_GameObject.HealthEffect;
+            }
+            else if(!temp_GameObject.IsHealthy)
+            {
+                temp_pet.Healt -= temp_GameObject.HealthEffect;
+            }
 
-
+            //update other stats
+            temp_pet.Hungry += temp_GameObject.HungryEffect;
+            temp_pet.Energy += temp_GameObject.EnergyEffect;
+            temp_pet.Hygene += temp_GameObject.HygeneEffect;
+            temp_pet.Study += temp_GameObject.StudyEffect;
         }
     }
 }
