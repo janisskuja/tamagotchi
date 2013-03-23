9dbdbfabed446349528abc01411aa0bbcccfccac
Lauris Radzevics <radzevics.lauris@gmail.com>
2013-03-23 20:37:00 +0200

    Added food and other status updaters.
    added things to encrease all statuses...
    This has some errors!!!!

--- VSProject/MetroTama/MetroTama/Domain/Repository/GameObjectRepository.cs
+++ VSProject/MetroTama/MetroTama/Domain/Repository/GameObjectRepository.cs
@@ -12,7 +12,11 @@ namespace MetroTama.Domain.Repository
             Dictionary<int, GameObject> objects = new Dictionary<int, GameObject>();
             objects.Add(1, getObject(1, "Apple"));
             objects.Add(2, getObject(2, "Burger"));
-            objects.Add(2, getObject(3, "Drink"));
+            objects.Add(3, getObject(3, "Drink"));
+            objects.Add(4, getObject(4, "Ball"));
+            objects.Add(5, getObject(5, "Read"));
+            objects.Add(6, getObject(6, "Clean"));
+            objects.Add(7, getObject(7, "Medic"));
             return objects[gameObjectId];
         }
 
@@ -21,13 +25,98 @@ namespace MetroTama.Domain.Repository
             GameObject gameObject = new GameObject();
             gameObject.GameObjectId = id;
             gameObject.Name = Name;
-            gameObject.IsHealthy = true;
-            gameObject.HungryEffect = 1;
-            gameObject.HealthEffect = 1;
-            gameObject.HygeneEffect = 1;
-            gameObject.FunEffect = 1;
-            gameObject.EnergyEffect = 1;
-            gameObject.StudyEffect = 1;
+
+            switch(id)
+            {
+                case 1: //Apple
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 10;
+                        gameObject.HealthEffect = 5;
+                        gameObject.HygeneEffect = 0;
+                        gameObject.FunEffect = 5;
+                        gameObject.EnergyEffect = 1;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+                case 2: //Burger
+                    {
+                        gameObject.IsHealthy = false;
+                        gameObject.HungryEffect = 25;
+                        gameObject.HealthEffect = 3;
+                        gameObject.HygeneEffect = -2;
+                        gameObject.FunEffect = 7;
+                        gameObject.EnergyEffect = 2;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+                case 3: //Drink
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 9;
+                        gameObject.HealthEffect = 2;
+                        gameObject.HygeneEffect = 0;
+                        gameObject.FunEffect = 2;
+                        gameObject.EnergyEffect = 5;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+                case 4: //Ball
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 0;
+                        gameObject.HealthEffect = 2;
+                        gameObject.HygeneEffect = -2;
+                        gameObject.FunEffect = 10;
+                        gameObject.EnergyEffect = -2;
+                        gameObject.StudyEffect = 1;
+                    }
+                    break;
+                case 5: //Read
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 0;
+                        gameObject.HealthEffect = 0;
+                        gameObject.HygeneEffect = 0;
+                        gameObject.FunEffect = 5;
+                        gameObject.EnergyEffect = 0;
+                        gameObject.StudyEffect = 20;
+                    }
+                    break;
+                case 6: //Clean
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 0;
+                        gameObject.HealthEffect = 0;
+                        gameObject.HygeneEffect = 40;
+                        gameObject.FunEffect = 0;
+                        gameObject.EnergyEffect = 0;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+                case 7: //Medic
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 0;
+                        gameObject.HealthEffect = 30;
+                        gameObject.HygeneEffect = 0;
+                        gameObject.FunEffect = 0;
+                        gameObject.EnergyEffect = 0;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+                default:
+                    {
+                        gameObject.IsHealthy = true;
+                        gameObject.HungryEffect = 0;
+                        gameObject.HealthEffect = 0;
+                        gameObject.HygeneEffect = 0;
+                        gameObject.FunEffect = 0;
+                        gameObject.EnergyEffect = 0;
+                        gameObject.StudyEffect = 0;
+                    }
+                    break;
+            }
             return gameObject;
         }
     }
